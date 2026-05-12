2ab11b38a3c5ea76e6b95b25dac6c337928e252a
CHAUHAN VAISHALI SURESHBHAI <vaishali@192.168.1.28>
2026-05-12 13:04:14 +0530

    redis setup

--- config/redis.js
+++ config/redis.js
@@ -1,11 +1,31 @@
 
-import {createClient} from "redis";
+// import {createClient} from "redis";
+
+// const redisClient = createClient({
+//     url: process.env.REDIS_URL || "redis://localhost:6379",
+// });
+
+// redisClient.on("error",(error)=>console.log(error));
+
+// await redisClient.connect();
+// export default redisClient;
+import { createClient } from "redis";
 
 const redisClient = createClient({
-    url:"redis://localhost:6379",
+    url: process.env.REDIS_URL || "redis://localhost:6379",
 });
 
-redisClient.on("error",(error)=>console.log(error));
+redisClient.on("error", (error) => {
+    console.log("Redis Error:", error);
+});
 
-await redisClient.connect();
+(async () => {
+    try {
+        await redisClient.connect();
+        console.log("Redis Connected");
+    } catch (error) {
+        console.log("Redis Connection Failed");
+    }
+})();
+
 export default redisClient;
